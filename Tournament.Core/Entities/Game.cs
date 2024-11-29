384271b7c1bdc8355b8a568e10181d7d85cb9a4e
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-29 14:33:14 +0100

    feat: add the data-attribute, add the status code

--- Tournament.Core/Entities/Game.cs
+++ Tournament.Core/Entities/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Tournament.Core.Entities
     public class Game
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(30, ErrorMessage = "The title cannot exceed 30 characters.")]
         public string Title { get; set; }
         public DateTime Time { get; set; }
         public string Description { get; set; }
--- Tournament.Core/Entities/TournamentModel.cs
+++ Tournament.Core/Entities/TournamentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace Tournament.Core.Entities
     public class TournamentModel
     {
        public int Id { get; set; }
-       public string Title { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "The title cannot exceed 30 characters.")]
+        public string Title { get; set; }
        public DateTime StartDate { get; set; }
 
         // Nav prop
